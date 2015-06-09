778ed83d3c349af2c2ada210749807ad54d199a4
dathims <dathims@gmail.com>
2015-06-10 01:53:03 +0200

    analytics + ellipsis icon and more

--- wp-content/themes/run2meet/footer.php
+++ wp-content/themes/run2meet/footer.php
@@ -43,6 +43,16 @@
 </div><!-- .site -->
 
 <?php wp_footer(); ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-41399980-3', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	<!-- Go to www.addthis.com/dashboard to customize your tools -->
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55755c226f534ada" async="async"></script>
 </body>
--- wp-content/themes/run2meet/index.php
+++ wp-content/themes/run2meet/index.php
@@ -312,12 +312,7 @@ get_header(); ?>
 
 				// End the loop.
 				endwhile;
-				// Previous/next page navigation.
-				the_posts_pagination( array(
-					'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-					'next_text'          => __( 'Next page', 'twentyfifteen' ),
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-				) );
+
 			// If no content, include the "No posts found" template.
 			else :
 				get_template_part( 'content', 'none' );
@@ -325,6 +320,15 @@ get_header(); ?>
 		?>
 
 			</div><!--.wrapper -->
+
+			<?php
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+				'next_text'          => __( 'Next page', 'twentyfifteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+			) );
+			?>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
